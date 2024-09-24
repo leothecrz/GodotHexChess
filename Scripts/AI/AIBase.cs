bba519284628b8d77aca61f29ea2eeaeb2a45887
Leonardo Davalos <32350741+leothecrz@users.noreply.github.com>
2024-09-24 14:34:45 -0700

    base ai finished

--- Scripts/AI/AIBase.cs
+++ Scripts/AI/AIBase.cs
@@ -1,6 +1,62 @@
 using Godot;
 using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 
 public partial class AIBase : Node
-{
+    {
+
+    private int side;
+    private Vector2I TO;
+    private Vector2I CORDS;
+    private int MOVETYPE;
+    private int MOVEINDEX;
+    private int PROMOTETO;
+
+    public AIBase(bool playswhite)
+    {
+        PROMOTETO = HexEngineSharp.PIECES.QUEEN;
+        side = 0;
+        if ( playswhite ) side = 1;
+        return;
+    }
+
+    
+    // GETTER
+
+    
+    public Vector2I _getCords()
+    {
+        return CORDS;
+    }
+
+    public int _getMoveType()
+    {
+        return MOVETYPE;
+    }
+
+    
+    public int _getMoveIndex()
+    {
+        return MOVEINDEX;
+    }
+
+    public int _getPromoteTo()
+    {
+        return PROMOTETO;
+    }
+
+    public int _getTo()
+    {
+        return TO;
+    }
+
+
+    
+
+
+    func _makeChoice(HexEngineSharp HexEngine)
+    {
+        return;
+    }
 }
