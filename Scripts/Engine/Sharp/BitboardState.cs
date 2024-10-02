36eba9f5cb7fc4ed0f606929885c758474dcf18a
Leonardo Davalos <32350741+leothecrz@users.noreply.github.com>
2024-10-01 23:51:46 -0700

    re-organizing state

---
+++ Scripts/Engine/Sharp/BitboardState.cs
@@ -0,0 +1,14 @@
+
+namespace HexChess
+{
+public class BitboardState
+{
+
+	private Bitboard128[] WHITE_BB;
+	private Bitboard128[] BLACK_BB;
+	private Bitboard128 BIT_WHITE;
+	private Bitboard128 BIT_BLACK;
+	private Bitboard128 BIT_ALL;
+
+}
+}
